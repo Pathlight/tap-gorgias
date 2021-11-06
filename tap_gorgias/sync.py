acb50855ec7c774dd6e4208d0b4ef4990d1c4861
Matt Behrens <matt@pathlight.com>
2021-11-06 13:22:51 -0700

    style cleanup around if bookmark exists

--- tap_gorgias/sync.py
+++ tap_gorgias/sync.py
@@ -10,13 +10,17 @@ def sync_stream(state, start_date, instance, config):
     stream = instance.stream
     mdata = stream.metadata
 
-    # If we have a bookmark, use it; otherwise use start_date & update bookmark with it
-    if (instance.replication_method == 'INCREMENTAL' and
-            not state.get('bookmarks', {}).get(stream.tap_stream_id, {}).get(instance.replication_key)):
-        singer.write_bookmark(state,
-                              stream.tap_stream_id,
-                              instance.replication_key,
-                              start_date)
+    # If we have a bookmark, use it;
+    # otherwise use start_date & update bookmark with it
+    bookmark_exists = (
+        state.get("bookmarks", {})
+        .get(stream.tap_stream_id, {})
+        .get(instance.replication_key)
+    )
+    if instance.replication_method == "INCREMENTAL" and not bookmark_exists:
+        singer.write_bookmark(
+            state, stream.tap_stream_id, instance.replication_key, start_date
+        )
 
     parent_stream = stream
     with metrics.record_counter(stream.tap_stream_id) as counter:
